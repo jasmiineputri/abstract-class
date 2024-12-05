a49d33228ed3d1f875ca5c062f320a5afd8287b1
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 14:01:40 +0700

    Update and rename Manusia.py to Manusia.java

---
+++ Manusia.java
@@ -0,0 +1,11 @@
+public class Manusia extends MakhlukHidup {
+    @Override
+    public String bernafas() {
+        return "Manusia bernafas dengan paru-paru.";
+    }
+
+    @Override
+    public String makan() {
+        return "Manusia makan dengan tangan.";
+    }
+}
--- Manusia.py
+++
@@ -1,6 +0,0 @@
-class Manusia(MakhlukHidup):
-    def bernafas(self):
-        return "Manusia bernafas dengan paru-paru."
-
-    def makan(self):
-        return "Manusia makan dengan tangan."
