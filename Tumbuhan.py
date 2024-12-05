990b4f0cdc409f352a539ac968bd232d88d44328
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 13:54:12 +0700

    Create Tumbuhan.py

---
+++ Tumbuhan.py
@@ -0,0 +1,6 @@
+class Tumbuhan(MakhlukHidup):
+    def bernafas(self):
+        return "Tumbuhan bernafas melalui stomata."
+
+    def makan(self):
+        return "Tumbuhan membuat makanan melalui fotosintesis."
