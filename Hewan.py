323f82bea39741786589d9c0708404d6c815f7f7
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 13:53:53 +0700

    Create Hewan.py

---
+++ Hewan.py
@@ -0,0 +1,6 @@
+class Hewan(MakhlukHidup):
+    def bernafas(self):
+        return "Hewan bernafas dengan paru-paru atau insang."
+
+    def makan(self):
+        return "Hewan makan dengan mulut atau paruh."
