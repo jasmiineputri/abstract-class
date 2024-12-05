8650f476b4cdd120f27ea65cd42e65aa5c9a6588
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 14:02:04 +0700

    Update and rename Tumbuhan.py to Tumbuhan.java

---
+++ Tumbuhan.java
@@ -0,0 +1,11 @@
+public class Tumbuhan extends MakhlukHidup {
+    @Override
+    public String bernafas() {
+        return "Tumbuhan bernafas melalui stomata.";
+    }
+
+    @Override
+    public String makan() {
+        return "Tumbuhan membuat makanan melalui fotosintesis.";
+    }
+}
--- Tumbuhan.py
+++
@@ -1,6 +0,0 @@
-class Tumbuhan(MakhlukHidup):
-    def bernafas(self):
-        return "Tumbuhan bernafas melalui stomata."
-
-    def makan(self):
-        return "Tumbuhan membuat makanan melalui fotosintesis."
