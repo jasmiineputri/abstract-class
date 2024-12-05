38553aa1176e8742687ef21619d6462e20cd2454
jasmiineputri <162336027+jasmiineputri@users.noreply.github.com>
2024-12-05 14:01:15 +0700

    Update and rename Hewan.py to Hewan.java

---
+++ Hewan.java
@@ -0,0 +1,11 @@
+public class Hewan extends MakhlukHidup {
+    @Override
+    public String bernafas() {
+        return "Hewan bernafas dengan paru-paru atau insang.";
+    }
+
+    @Override
+    public String makan() {
+        return "Hewan makan dengan mulut atau paruh.";
+    }
+}
--- Hewan.py
+++
@@ -1,6 +0,0 @@
-class Hewan(MakhlukHidup):
-    def bernafas(self):
-        return "Hewan bernafas dengan paru-paru atau insang."
-
-    def makan(self):
-        return "Hewan makan dengan mulut atau paruh."
